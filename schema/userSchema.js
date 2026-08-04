c2446857849f57b4b3661142e4f27260706bd27a
Eazybel <eazybel27@gmail.com>
2026-08-05 00:30:51 +0300

    refactor: update response handling in script.js; remove unused chatHistorySchema and userSchema

--- schema/userSchema.js
+++
@@ -1,5 +0,0 @@
-const mongoose=require("mongoose")
-const user=new mongoose.Schema({"email":{type:String}})
-const userModel=new mongoose.model("userModel",user,"users")
-module.exports=userModel
-
--- server.js
+++ server.js
@@ -13,8 +13,6 @@ app.use(express.urlencoded({extended:true}))
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname,"./public/index.html"))
 })
-app.post("/historyUpdate",historyUpdate)
-app.post("/userController",userController)
 const port = process.env.PORT || 5000
 app.listen(port, () => {
     console.log(`SERVER RUNNING ON ${port}`)
